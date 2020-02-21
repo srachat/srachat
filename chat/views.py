fd600ca7c3452ea844ee496f2c6f7ee6d126f5e1
Pavel Paranin <pavel.paranin@blindspot.ai>
2020-02-21 15:12:46 +0100

    Base and chat rooms page templates are added.\nViews for those templates are added as well

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def chat_rooms(request):
+    rooms = Room.objects.all()
+    return render(request, "chat/chat_room_selector.html", {"rooms": rooms})
