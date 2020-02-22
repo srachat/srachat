02d54724c59d10ba1d76702a2e4b0f56ef2db4c8
Artem Jasan <artjasan@gmail.com>
2020-02-22 13:49:43 +0100

    Basic project setup

--- srachat/settings.py
+++ srachat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Information about the location static files (for the future)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
