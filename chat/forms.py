0b088323877ccea5d5089e6e61ad43463be23407
Artem Jasan <artjasan@gmail.com>
2020-03-05 22:44:41 +0100

    Creating a new form -  CommentForm and single chat room

--- chat/forms.py
+++ chat/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from chat.models import Room
+from chat.models import Comment, Room
 
 
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
