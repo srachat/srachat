1eb8699ad91b28fe874cb16a39de766edb7fb37b
Pavel Paranin <pavel.paranin@blindspot.ai>
2020-03-25 23:32:48 +0100

    Custom ChatUser model is added. Static methods for object fetching are added

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
 
 
 class Room(models.Model):
     title = models.CharField(max_length=250, unique=True)
+
+    @staticmethod
+    def get_room_or_404(pk):
+        return get_object_or_404(Room, pk=pk)
 
     def __str__(self):
         return self.title
@@ -16,5 +24,29 @@ class Comment(models.Model):
     class Meta:
         ordering = ('created',)
 
+    @staticmethod
+    def get_comment_or_404(pk):
+        return get_object_or_404(Comment, pk=pk)
+
     def __str__(self):
         return self.body
+
+
+class ChatUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(Room, related_name='chat_users')
+
+    @staticmethod
+    def get_chat_user_or_404(pk):
+        return get_object_or_404(ChatUser, pk=pk)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        ChatUser.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.chatuser.save()
