f6b05dc7da0b91853ebcfa1f30824b86238bf2dc
Pavel Paranin <pavel.paranin@blindspot.ai>
2020-03-25 23:36:17 +0100

    Existence of room underlying comments is checked before request evaluation

--- chat/views/comments.py
+++ chat/views/comments.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,16 +12,17 @@ class CommentList(APIView):
     or if the room id is given to display or add comments to the given room.
     """
     def get(self, request, pk, format=None):
-        comments = Comment.objects.filter(room=pk)
+        room = Room.get_room_or_404(pk)
+        comments = Comment.objects.filter(room=room)
         serializer = SingleRoomCommentSerializer(comments, many=True)
         return Response(serializer.data)
 
     def post(self, request, pk, format=None):
+        room = Room.get_room_or_404(pk)
         serializer = SingleRoomCommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(room=Room.objects.get(pk=pk))
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(room=room)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentDetail(APIView):
@@ -30,23 +30,22 @@ class CommentDetail(APIView):
     This view is able to display, update and delete a single comment.
     If no room is specified in the url path, it should be given inside request body.
     """
-    def get_comment(self, pk):
-        return get_object_or_404(Comment, pk=pk)
-
     def get(self, request, room_pk, comment_pk, format=None):
-        comment = self.get_comment(comment_pk)
+        room = Room.get_room_or_404(room_pk)
+        comment = Comment.get_comment_or_404(comment_pk)
         serializer = SingleRoomCommentSerializer(comment)
         return Response(serializer.data)
 
     def put(self, request, room_pk, comment_pk, format=None):
-        comment = self.get_comment(comment_pk)
+        room = Room.get_room_or_404(room_pk)
+        comment = Comment.get_comment_or_404(comment_pk)
         serializer = SingleRoomCommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, room_pk, comment_pk, format=None):
-        comment = self.get_comment(comment_pk)
+    def delete(self, request, room_pk, comment_pk):
+        room = Room.get_room_or_404(room_pk)
+        comment = Comment.get_comment_or_404(comment_pk)
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
