3b9422b4bd11fba16b23489adf86ab6ea240956b
Pavel Paranin <pavel.paranin@blindspot.ai>
2020-04-02 22:51:17 +0200

    only registered user can create room. only registered creator can change rooms settings. when room is created, chatuser is added as a creator

---
+++ chat/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+from chat.models import ChatUser
+
+
+class IsCreatorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow creators of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.creator == ChatUser.objects.get(user=request.user)
--- chat/views/rooms.py
+++ chat/views/rooms.py
@@ -1,21 +1,36 @@
-from rest_framework import viewsets, generics
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
-from chat.models import Room
+from chat.models import Room, ChatUser
+from chat.permissions import IsCreatorOrReadOnly
 from chat.serializers.room_serializer import RoomSerializer
 
 
-class RoomList(generics.ListCreateAPIView):
+class RoomList(
+    generics.CreateAPIView,
+    generics.ListAPIView
+):
     """
     This view is able to display all existing rooms
     or to create a new one.
     """
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(creator=ChatUser.objects.get(user=request.user))
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class RoomDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     This view is able to display, update and delete a single room.
     """
+    permission_classes = [IsAuthenticatedOrReadOnly & IsCreatorOrReadOnly]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
