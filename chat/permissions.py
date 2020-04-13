7bc46b8df9dc6ad056a7297352a68f42af8f2134
Pavel Paranin <pavel.paranin@blindspot.ai>
2020-04-13 17:14:08 +0200

    room participant permission is added

--- chat/permissions.py
+++ chat/permissions.py
@@ -14,5 +14,19 @@ class IsCreatorOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
+        # Write permissions are only allowed to the creator of the object.
         return obj.creator == ChatUser.objects.get(user=request.user)
+
+
+class IsRoomParticipantOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only room participants to leave comments
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return ChatUser.objects.get(user=request.user) in obj.chat_users.all()
