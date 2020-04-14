5c9f1371632ce7814884c281db7eeb2c7a2ca6e1
Artem Jasan <artjasan@gmail.com>
2020-04-14 22:46:41 +0200

    New custom permission for the Account Owner

--- chat/permissions.py
+++ chat/permissions.py
@@ -16,3 +16,31 @@ class IsCreatorOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.creator == ChatUser.objects.get(user=request.user)
+
+
+class IsRoomParticipantOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only room participants to leave comments
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return ChatUser.objects.get(user=request.user) in obj.chat_users.all()
+
+
+class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only room participants to leave comments
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
