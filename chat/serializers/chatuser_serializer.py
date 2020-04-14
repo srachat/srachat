083729bf64d029f4ed13ca7831c15982b78336ec
Artem Jasan <artjasan@gmail.com>
2020-04-14 22:49:08 +0200

    Update of the ChatUserSerializer and assing of the custom permission to the ChatUserDetail view

--- chat/serializers/chatuser_serializer.py
+++ chat/serializers/chatuser_serializer.py
@@ -1,17 +1,28 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from chat.models import ChatUser
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("last_login", "username", "first_name", "last_name", "date_joined")
-
-
 class ChatUserSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    last_login = serializers.CharField(source='user.last_login', read_only=True)
+    username = serializers.CharField(source='user.username')
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    date_joined = serializers.CharField(source='user.date_joined', read_only=True)
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.get('user', {})
+        image_data = validated_data.get('image', instance.image)
+
+        instance.user.username = user_data.get("username", instance.user.username)
+        instance.user.first_name = user_data.get("first_name", instance.user.first_name)
+        instance.user.last_name = user_data.get("last_name", instance.user.last_name)
+        instance.image = image_data
+
+        instance.user.save()
+        instance.save()
+
+        return instance
 
     class Meta:
         model = ChatUser
-        fields = '__all__'
+        exclude = ('user',)
