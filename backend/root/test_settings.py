86f5581f0180acd419078ce70d1e126aa8368f27
Pavel Paranin <pavel.paranin@firma.seznam.cz>
2020-09-27 20:10:51 +0200

    db fix for testing

--- backend/root/test_settings.py
+++ backend/root/test_settings.py
@@ -2,4 +2,7 @@ from .settings import *
 
 # make tests faster
 SOUTH_TESTS_MIGRATE = False
-DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
