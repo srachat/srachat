8a4cc59f41bf26097b43d638bcf9f310ce928ffe
Pavel Paranin <pavel.paranin@firma.seznam.cz>
2020-10-13 18:18:48 +0200

    testing of post and other not safe methods is added

--- backend/srachat/tests/test_tags_endpoints.py
+++ backend/srachat/tests/test_tags_endpoints.py
@@ -9,13 +9,19 @@ from ..tests.utils import SrachatTestCase
 
 
 class TagsTest(SrachatTestCase):
+    def setUp(self) -> None:
+        self.url = reverse("list_tags")
+
     def test_all_tags_fetching(self):
         """
             GET: '/pidor/tags/'
         """
-        url = reverse("list_tags")
-
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(len(response.data) > 0)
         self.assertIn("name", response.data[0].keys())
+
+    def test_only_get_is_allowed(self):
+        self.check_safe_methods(self.url)
+        response_post = self.client.post(self.url)
+        self.assertEqual(response_post.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
